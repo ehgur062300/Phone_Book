d3e540432dfcf9e1dbb30fa74e98678bab5c986e
ehgur062300 <79880998+ehgur062300@users.noreply.github.com>
2021-12-02 16:51:54 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,44 +2,50 @@
 #include "DLL.h"
 
 using namespace std;
-
 int main() {
+	system(" mode  con lines=30   cols=60 ");
+
 	int num = 0;
 	string data;
 	string data_name, data_num;
 	struct DLL dll;
 
-	dll.Print_data();
 	while (1) {
 		cout << "---------------------------------------------------------\n";
-		cout << "<< 1. 검색 >>  << 2. 수정 >>  << 3. 삭제 >>  << 4. 추가 >>\n";
+		cout << "< 1. 검색 > < 2. 수정 > < 3. 삭제 > < 4. 추가 > <5. 목록>\n";
 		cout << "---------------------------------------------------------\n";
 
 		switch (dll.get_int()) {
 		case 1:
 			system("cls");
 			dll.Search_Node();
-			dll.Print_data();
 			break;
 
 		case 2:
 			system("cls");
 			dll.Modify_Node();
-			dll.Print_data();
 			break;
 
 		case 3:
 			system("cls");
 			dll.Delete_Node();
-			dll.Print_data();
 			break;
 
 		case 4:
-			system("cls");
 			dll.Insert_Node();
 			dll.Print_data();
 			break;
+
+		case 5:
+			system("cls");
+			dll.Print_data();
+			break;
+
+		default:
+			cout << "잘못입력하셨습니다.\n";
+			break;
 		}
+		
 	}
 	return 0;
 }
